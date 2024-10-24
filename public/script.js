ee9d52ca7dba88073f836a29973bd68edf02cf71
Dennis Dechkevich <ddechkevich@me.com>
2024-10-24 10:43:59 -0500

    remove extra

--- public/script.js
+++ public/script.js
@@ -97,7 +97,7 @@ function buildTeamTable(data, team, teamLabel, maxRows) {
             // Store row content in a hidden <textarea>
             tableContent += `${player.name}:\tC:[${player.stolb.toFixed(2)}|${
                 data.max_allowed
-            }]\tK:${player.barrier}\tЗ:${player.snake}\tB:${player.will}\n`
+            } ]\n`
 
             return row
         })
@@ -147,7 +147,7 @@ function buildTeamTable(data, team, teamLabel, maxRows) {
                     ${emptyRows} <!-- Add empty rows to balance the table -->
                 </tbody>
             </table>
-            <textarea id="team-${team}-data" class="hidden-textarea">${tableContent}\nDead people: ${data.los_muertos[
+            <textarea id="team-${team}-data" class="hidden-textarea">${tableContent}Dead people: ${data.los_muertos[
         team
     ].join(", ")}\n</textarea>
             <button class="copy-button" onclick="copyFromTextarea('team-${team}-data', this)">Копировать</button>
